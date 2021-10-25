12636e48d9386e975f538b01fca59083b6b38cb5
amjay3676 <nojjymj81@gmail.com>
2021-10-25 14:31:32 +0530

    first

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+//routes
+app.get("/", (req, res) => {
+    res.send("This is home route");
+});
+
+app.get("/about", (req, res) => {
+    res.send("this is about route")
+})
+app.listen(PORT, () =>{
+    console.log("server is runnng on port ",+ PORT);
+});
